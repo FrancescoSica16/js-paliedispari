6ad6e2b5c133232a21fdf0b1bb6069487df6cc30
FrancescoSica16 <francesco.sica16@gmail.com>
2021-08-31 18:20:34 +0200

    def

--- myscript.js
+++ myscript.js
@@ -1,54 +1,62 @@
-// const parola = prompt("dammi una parola");
 
-// let reverseParola = "" ;
-
-// let verifica = isPalindroma(parola , reverseParola);
-
-// console.log(verifica);
-
-// function isPalindroma(x , y) {
-
-    
-//     for (let i = x.length -1 ; i >= 0 ; i--) {
+const parola = prompt("dammi una parola");
+ let reverseParola = "" ;
+ let verifica = isPalindroma(parola , reverseParola);
+ console.log(verifica);
+ function isPalindroma(x , y) {
+  
+     for (let i = x.length -1 ; i >= 0 ; i--) {
+      
+      y += x.charAt(i);  
+   
+     }
         
-//      y += x.charAt(i);  
-     
-//     }
-          
-//     if (x == y) {
-//         return ("la parola è palindroma");
-//     }
-//     else {
-//         return ("la parola NON è palindroma");
-//     }
-// }
+     if (x == y) {
+         return ("la parola è palindroma");
+     }
+     else {
+         return ("la parola NON è palindroma");
+     }
+ }
 
 // PARI O DISPARI
 
 let somma = 0 ;
 
-let pariODispari = prompt("scegli se pari o dispari").toUpperCase;
-
-
-if ((pariODispari == "PARI" || pariODispari == "DISPARI")) {
-    alert("NON FARE IL FURBO");
-} 
+let pariODispari = prompt("scegli se pari o dispari").toUpperCase();
 
 let numeroScelto = parseInt(prompt("adesso scegli un numero da 1 a 5"));
 
-let numPc = numRandom (1 , 5);
+console.log(pariODispari)
+console.log(numeroScelto);
 
-console.log(numPc)
+if ((pariODispari == "PARI" || pariODispari == "DISPARI") && (numeroScelto >= 1 && numeroScelto <=5)) {
+   
+    let numPc = numRandom (1 , 5);
 
-if (!(numeroScelto >= 1 && numeroScelto <= 5)) {
-    
-    alert("non hai inserito un numero tra 1 e 5");
+    console.log(numPc)
 
-} else{
+    // if (!(numeroScelto >= 1 && numeroScelto <= 5)) {
+        
+    //     alert("non hai inserito un numero tra 1 e 5");
+
     somma += numeroScelto + numPc;
-} 
 
-console.log(somma);
+    console.log(somma);
+
+    const pari = isEven(somma);
+
+    if ((pariODispari == "PARI" && pari == true) || (pariODispari == "DISPARI" && pari == false)) {
+        console.log("hai vinto");
+
+    } else {
+        console.log("hai perso");
+    }
+
+    
+} else { 
+    alert("non hai inserito un numero tra 1 e 5 OPPURE NON HAI SCELTO TRA PARI O DISPARI");
+        }
 
 
 function isEven(value) {
